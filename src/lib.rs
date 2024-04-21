4752a87031af678a378dbbda7d351b3fa82482a9
amatgil <amatgilvinyes@gmail.com>
2024-04-21 21:12:29 +0200

    Started sequence rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,34 @@ pub mod colors;
 pub use colors::*;
 
 
+// Position constants
+const P_TOP_RIGHT_FRONT: usize    = 0;
+const P_TOP_RIGHT_BACK: usize     = 1;
+const P_TOP_LEFT_BACK: usize      = 2;
+const P_TOP_LEFT_FRONT: usize     = 3;
+const P_BOTTOM_RIGHT_FRONT: usize = 4;
+const P_BOTTOM_RIGHT_BACK: usize  = 5;
+const P_BOTTOM_LEFT_BACK: usize   = 6;
+const P_BOTTOM_LEFT_FRONT: usize  = 7;
+
+const SIDE_RIGHT: usize = 0;
+const SIDE_FRONT: usize = 1;
+const SIDE_TOP: usize   = 2;
+const SIDE_LEFT: usize  = 3;
+const SIDE_BACK: usize  = 4;
+const SIDE_DOWN: usize  = 5;
+ 
 
 // Rotation constants
-const COLOR_RIGHT_SEQ: [usize; 4] = [1, 2, 4, 5];
-const COLOR_LEFT_SEQ: [usize; 4]  = [5, 4, 2, 1];
-const COLOR_UP_SEQ: [usize; 4]    = [0, 1, 3, 4];
-const COLOR_DOWN_SEQ: [usize; 4]  = [4, 3, 1, 0];
-const COLOR_FRONT_SEQ: [usize; 4] = [0, 5, 3, 2];
-const COLOR_BACK_SEQ: [usize; 4]  = [2, 3, 5, 0];
+const COLOR_RIGHT_SEQ: [usize; 4] = [SIDE_FRONT, SIDE_TOP, SIDE_BACK, SIDE_DOWN];
+const COLOR_LEFT_SEQ: [usize; 4]  = [SIDE_DOWN, SIDE_BACK, SIDE_TOP, SIDE_FRONT];
+const COLOR_UP_SEQ: [usize; 4]    = [SIDE_FRONT, SIDE_LEFT, SIDE_BACK, SIDE_RIGHT];
+const COLOR_DOWN_SEQ: [usize; 4]  = [SIDE_RIGHT, SIDE_BACK, SIDE_LEFT, SIDE_FRONT];
+const COLOR_FRONT_SEQ: [usize; 4] = [SIDE_TOP, SIDE_RIGHT, SIDE_DOWN, SIDE_LEFT];
+const COLOR_BACK_SEQ: [usize; 4]  = [SIDE_LEFT, SIDE_DOWN, SIDE_RIGHT, SIDE_TOP];
 
-const FACE_RIGHT_SEQ_CYCLE: [usize; 4] = [0, 1, 5, 4];
-const FACE_LEFT_SEQ_CYCLE: [usize; 4]  = [3, 7, 6, 2];
+const FACE_RIGHT_SEQ_CYCLE: [usize; 4] = [P_TOP_RIGHT_BACK, P_BOTTOM_RIGHT_BACK, P_BOTTOM_RIGHT_FRONT, P_TOP_RIGHT_FRONT];
+const FACE_LEFT_SEQ_CYCLE: [usize; 4]  = [P_TOP_LEFT_FRONT, P_BOTTOM_LEFT_FRONT, P_BOTTOM_LEFT_BACK, P_TOP_LEFT_BACK];
 const FACE_UP_SEQ_CYCLE: [usize; 4]    = [2, 1, 0, 3];
 const FACE_DOWN_SEQ_CYCLE: [usize; 4]  = [7, 4, 5, 6];
 const FACE_FRONT_SEQ_CYCLE: [usize; 4] = [3, 0, 4, 7];
