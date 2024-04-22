963b199d7df917af0010911bf04bb6e80325054a
amatgil <amatgilvinyes@gmail.com>
2024-04-23 01:30:47 +0200

    SeqMoves seems to print properly

--- src/declarations.rs
+++ src/declarations.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::*;
 
 #[derive(Debug, Clone, Copy, Hash, Default)]
@@ -104,6 +106,146 @@ impl Cube {
     }
  
 }
+
+#[derive(Debug, Clone)]
+pub struct MoveSeq(pub Vec<Move>);
+
+impl From<Vec<Move>> for MoveSeq { fn from(value: Vec<Move>) -> Self { Self(value) } }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ExpandedMove {
+    L { prime: bool }, L2,
+    R { prime: bool }, R2,
+    F { prime: bool }, F2,
+    B { prime: bool }, B2,
+    U { prime: bool }, U2,
+    D { prime: bool }, D2,
+    Nothing
+}
+impl ExpandedMove {
+    /// This preserves rotations: so R L *could* be rewritten as 2L or 2R while preserving equality but, since it does not preserve rotation, it will not be compressed
+    fn compress(a: Self, b: Self) -> Option<Self> {
+	match (a, b) {
+	    (Self::Nothing, _)
+		| (_, Self::Nothing)
+		=> None,
+	    (Self::L { prime: p1 }, Self::L { prime: p2 } ) 
+		| (Self::R { prime: p1 }, Self::R { prime: p2 } )
+		| (Self::F { prime: p1 }, Self::F { prime: p2 } )
+		| (Self::B { prime: p1 }, Self::B { prime: p2 } )
+		| (Self::B { prime: p1 }, Self::B { prime: p2 } ) if p1 != p2
+		=> Some(Self::Nothing),
+	    (Self::L2, Self::L2)
+		| (Self::R2, Self::R2)
+		| (Self::F2, Self::D2)
+		| (Self::B2, Self::B2)
+		| (Self::U2, Self::U2)
+		| (Self::D2, Self::D2)
+		=> Some(Self::Nothing),
+	    (Self::L { prime: p1 }, Self::L { prime: p2} ) if p1 == p2 => Some(Self::L2),
+	    (Self::R { prime: p1 }, Self::R { prime: p2} ) if p1 == p2 => Some(Self::R2),
+	    (Self::F { prime: p1 }, Self::F { prime: p2} ) if p1 == p2 => Some(Self::F2),
+	    (Self::B { prime: p1 }, Self::B { prime: p2} ) if p1 == p2 => Some(Self::B2),
+	    (Self::U { prime: p1 }, Self::U { prime: p2} ) if p1 == p2 => Some(Self::U2),
+	    (Self::D { prime: p1 }, Self::D { prime: p2} ) if p1 == p2 => Some(Self::D2),
+	    (Self::L { prime: p1 }, Self::L2) => Some(Self::L { prime: !p1 }),
+	    (Self::R { prime: p1 }, Self::R2) => Some(Self::R { prime: !p1 }),
+	    (Self::F { prime: p1 }, Self::F2) => Some(Self::F { prime: !p1 }),
+	    (Self::B { prime: p1 }, Self::B2) => Some(Self::B { prime: !p1 }),
+	    (Self::U { prime: p1 }, Self::U2) => Some(Self::U { prime: !p1 }),
+	    (Self::D { prime: p1 }, Self::D2) => Some(Self::D { prime: !p1 }),
+	    (Self::L2, Self::L { prime: p1 }) => Some(Self::L { prime: !p1 }),
+	    (Self::R2, Self::R { prime: p1 }) => Some(Self::R { prime: !p1 }),
+	    (Self::F2, Self::F { prime: p1 }) => Some(Self::F { prime: !p1 }),
+	    (Self::B2, Self::B { prime: p1 }) => Some(Self::B { prime: !p1 }),
+	    (Self::U2, Self::U { prime: p1 }) => Some(Self::U { prime: !p1 }),
+	    (Self::D2, Self::D { prime: p1 }) => Some(Self::D { prime: !p1 }),
+	    _ => None
+	}
+    }
+} 
+
+impl Display for ExpandedMove {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+	let is_p = |&b| if b { "'" } else { "" };
+	let o: String = match self {
+	    ExpandedMove::L { prime } => format!("L{}", is_p(prime)),
+	    ExpandedMove::R { prime } => format!("R{}", is_p(prime)),
+	    ExpandedMove::F { prime } => format!("F{}", is_p(prime)),
+	    ExpandedMove::B { prime } => format!("B{}", is_p(prime)),
+	    ExpandedMove::U { prime } => format!("U{}", is_p(prime)),
+	    ExpandedMove::D { prime } => format!("D{}", is_p(prime)),
+	    ExpandedMove::L2 => "L2".into(),
+	    ExpandedMove::R2 => "R2".into(),
+	    ExpandedMove::F2 => "F2".into(),
+	    ExpandedMove::B2 => "B2".into(),
+	    ExpandedMove::U2 => "U2".into(),
+	    ExpandedMove::D2 => "D2".into(),
+	    ExpandedMove::Nothing => "".into(),
+	};
+	write!(f, "{o}")
+    }
+}
+
+impl Display for MoveSeq {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+	let mut stack: Vec<ExpandedMove> = vec![];
+	for mov in self.0.iter() {
+	    let ext = match mov {
+		Move { side: MoveSide::L , prime } => ExpandedMove::L { prime: *prime },
+		Move { side: MoveSide::R , prime } => ExpandedMove::R { prime: *prime },
+		Move { side: MoveSide::F , prime } => ExpandedMove::F { prime: *prime },
+		Move { side: MoveSide::B , prime } => ExpandedMove::B { prime: *prime },
+		Move { side: MoveSide::U , prime } => ExpandedMove::U { prime: *prime },
+		Move { side: MoveSide::D , prime } => ExpandedMove::D { prime: *prime },
+	    };
+	    stack.push(ext);
+	    let mut running = true;
+	    while stack.len() > 1 && running {
+		if let Some(c) = ExpandedMove::compress(
+		    stack[stack.len() - 2],
+		    stack[stack.len() - 1],
+		) {
+		    stack.pop();
+		    stack.pop();
+		    stack.push(c);
+		} else { break; }
+	    }
+	}
+	let mut o = String::new();
+	for m in stack {
+	    if m != ExpandedMove::Nothing { o.push_str(&format!("{} ", m)) };
+	}
+
+	write!(f, "{o}")
+    }
+}
+
+//#[test]
+//fn moveseq_debuggign() {
+//    let (_, random_moves) = Cube::random_scramble(6); 
+//
+//    let random_moves = vec![
+//	Move::new("R"),
+//	Move::new("R"),
+//	Move::new("R"),
+//	Move::new("R"),
+//	Move::new("R"),
+//	Move::new("R"),
+//	Move::new("L"),
+//	Move::new("L"),
+//	Move::new("L"),
+//	Move::new("R"),
+//	Move::new("L"),
+//	Move::new("R"),
+//	Move::new("L"),
+//    ];
+//    print_solution(&random_moves);
+//    let seq: MoveSeq = random_moves.into();
+//    println!("Compressed: {seq}");
+//
+//    panic!()
+//}
 
 #[test]
 fn redundant_move_right() {
