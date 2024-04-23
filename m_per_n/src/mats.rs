6fd2337d67e57b52275b665298b4b26695e00225
amatgil <amatgilvinyes@gmail.com>
2024-04-23 20:57:57 +0200

    Reinstated proper main, update needs re-doing

--- m_per_n/src/mats.rs
+++ m_per_n/src/mats.rs
@@ -19,11 +19,25 @@ impl<const NROWS: usize> Mul<f64> for MatRow<NROWS> {
     fn mul(self, lambda: f64) -> Self { MatRow::<NROWS>(self.0.map(|i| i*lambda)) }
 }
 
+impl<const NROWS: usize> Mul<MatRow<NROWS>> for f64 {
+    type Output = MatRow<NROWS>;
+    fn mul(self, rhs: MatRow<NROWS>) -> Self::Output { MatRow::<NROWS>(rhs.0.map(|i| i*self)) }
+}
+
 impl<const NROWS: usize> Add for MatRow<NROWS> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
         let mut out = [0.0; NROWS];
         for i in 0..NROWS { out[i] = self.0[i] + rhs.0[i]}
+        MatRow::<NROWS>(out)
+    }
+}
+
+impl<const NROWS: usize> Sub for MatRow<NROWS> { 
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        let mut out = [0.0; NROWS];
+        for i in 0..NROWS { out[i] = self.0[i] - rhs.0[i]}
         MatRow::<NROWS>(out)
     }
 }
@@ -37,7 +51,7 @@ impl<const N: usize> IndexMut<usize> for MatRow<N> {
     fn index_mut(&mut self, idx: usize) -> &mut Self::Output { &mut self.0[idx] }
 }
 
-// Generic (rectangular) impls
+/// Generic (rectangular) impls
 impl<const NF: usize, const NC: usize> Matrix<NF, NC> {
     #[allow(non_snake_case)]
     pub const fn ZERO() -> Matrix<NF, NC> {
@@ -51,6 +65,7 @@ impl<const N: usize> Matrix<N, N> {
     #[allow(non_snake_case)]
     pub const fn ID() -> Matrix<N, N> {
         let mut out = Matrix::<N, N>([MatRow::<N>([0.0; N]); N]);
+
         let mut i = 0;
         while i < N {
             out.0[i].0[i] = 1.0;
@@ -58,22 +73,31 @@ impl<const N: usize> Matrix<N, N> {
         }
         out
     }
-    pub const fn scalar_matrix(lambda: f64) -> Matrix<N, N> {
-        let mut out = Matrix::<N, N>([MatRow::<N>([0.0; N]); N]);
-        let mut i = 0;
-        while i < N {
-            out.0[i].0[i] = lambda;
-            i += 1;
-        }
-        out
-    }
 
-    pub fn inverse(&self) -> Self {
+    pub fn inverse(&self) -> Option<Self> {
         let mut inverse = Matrix::<N, N>::ID();
 
-	todo!()
+        inverse[1] = inverse[0][0] * inverse[1] - inverse[1][0] * inverse[0];
+        inverse[2] = inverse[0][0] * inverse[2] - inverse[2][0] * inverse[0];
+
+        inverse[0] = inverse[1][1] * inverse[0] - inverse[0][1] * inverse[1];
+        inverse[2] = inverse[1][1] * inverse[2] - inverse[2][1] * inverse[1];
+
+        inverse[0] = inverse[2][2] * inverse[0] - inverse[0][2] * inverse[2];
+        inverse[1] = inverse[2][2] * inverse[1] - inverse[1][2] * inverse[2];
+
+
+        if inverse[0][0] == 0.0 || inverse[1][1] == 0.0 || inverse[2][2] == 0.0 {
+            return None;
+        }
+        inverse[0] = 1.0/inverse[0][0] * inverse[0];
+        inverse[1] = 1.0/inverse[1][1] * inverse[1];
+        inverse[2] = 1.0/inverse[2][2] * inverse[2];
+
+        Some(inverse)
     }
 }
+
 
 /// Matrix addition (must have the same dimensions, enforced by type-system)
 impl<const NF: usize, const NC: usize> Add<Matrix<NF, NC>> for Matrix<NF, NC> {
@@ -144,107 +168,6 @@ impl<const NF: usize, const NC: usize> Index<usize> for Matrix<NF, NC> {
 impl<const NF: usize, const NC: usize> IndexMut<usize> for Matrix<NF, NC> {
     fn index_mut(&mut self, idx: usize) -> &mut Self::Output { 
         &mut self.0[idx]
-    }
-}
-
-// Generic impls
-impl<const NF: usize, const NC: usize> Matrix<NF, NC> {
-    #[allow(non_snake_case)]
-    pub const fn ZERO() -> Matrix<NF, NC> {
-        Matrix::<NF, NC>([MatRow::<NC>([0.0; NC]); NF])
-    }
-
-}
-
-// Square impls
-impl<const N: usize> Matrix<N, N> {
-    #[allow(non_snake_case)]
-    pub const fn ID() -> Matrix<N, N> {
-        let mut out = Matrix::<N, N>([MatRow::<N>([0.0; N]); N]);
-        let mut i = 0;
-        while i < N {
-            out.0[i].0[i] = 1.0;
-            i += 1;
-        }
-        out
-    }
-
-    pub fn inverse(&self) -> Option<Self> {
-        let mut inverse = Matrix::<N, N>::ID();
-
-        inverse[1] = inverse[0][0] * inverse[1] - inverse[1][0] * inverse[0];
-        inverse[2] = inverse[0][0] * inverse[2] - inverse[2][0] * inverse[0];
-
-        inverse[0] = inverse[1][1] * inverse[0] - inverse[0][1] * inverse[1];
-        inverse[2] = inverse[1][1] * inverse[2] - inverse[2][1] * inverse[1];
-
-        inverse[0] = inverse[2][2] * inverse[0] - inverse[0][2] * inverse[2];
-        inverse[1] = inverse[2][2] * inverse[1] - inverse[1][2] * inverse[2];
-
-
-        if inverse[0][0] == 0.0 || inverse[1][1] == 0.0 || inverse[2][2] == 0.0 {
-            return None;
-        }
-        inverse[0] = 1.0/inverse[0][0] * inverse[0];
-        inverse[1] = 1.0/inverse[1][1] * inverse[1];
-        inverse[2] = 1.0/inverse[2][2] * inverse[2];
-
-        Some(inverse)
-    }
-}
-
-impl<const NF: usize, const NC: usize> Add<Matrix<NF, NC>> for Matrix<NF, NC> {
-    type Output = Matrix<NF, NC>;
-    fn add(mut self, rhs: Self) -> Self::Output {
-        for y in 0..NF {
-            for x in 0..NC { self.0[y][x] += rhs.0[y][x]; }
-        }
-        self
-    }
-}
-
-impl<const NF: usize, const NC: usize> Sub<Matrix<NF, NC>> for Matrix<NF, NC> {
-    type Output = Matrix<NF, NC>;
-    fn sub(mut self, rhs: Self) -> Self::Output {
-        for y in 0..NF {
-            for x in 0..NC { self.0[y][x] -= rhs.0[y][x] }
-        }
-        self
-    }
-}
-
-impl<const NF: usize, const NC: usize> Mul<f64> for Matrix<NF, NC> {
-    type Output = Matrix<NF, NC>;
-    fn mul(mut self, rhs: f64) -> Self::Output {
-        for y in 0..NF {
-            for x in 0..NC { self.0[y][x] = self.0[y][x]*rhs }
-        }
-        self
-    }
-}
-
-impl<const NF: usize, const NC: usize> Mul<Matrix<NF, NC>> for f64 {
-    type Output = Matrix<NF, NC>;
-    fn mul(self, mut rhs: Matrix<NF, NC>) -> Self::Output {
-        for y in 0..NF {
-            for x in 0..NC { rhs.0[y][x] = rhs.0[y][x]*self }
-        }
-        rhs
-    }
-}
-
-impl<
-    const N: usize,
-    const C: usize, // Unused
-    const F: usize  // Unused
-> Mul<Matrix<N, C>> for Matrix<F, N> {
-    type Output = Matrix<F, C>;
-    fn mul(self, rhs: Matrix<N, C>) -> Self::Output {
-        let mut out: Self::Output = Matrix::ZERO();
-        for y in 0..F {
-            for x in 0..C { out.0[y][x] = (0..N).map(|i| self.0[y][i]*rhs.0[i][x]).sum() }
-        }
-        out
     }
 }
 
@@ -418,5 +341,5 @@ fn inverse_id() {
             [0.0, 1.0, 0.0].into(),
             [0.0, 0.0, 1.0].into()]
     );
-    assert_eq!(id.inverse().0, correct_id.0);
+    compare_mats(id.inverse().unwrap().0, correct_id.0);
 }
