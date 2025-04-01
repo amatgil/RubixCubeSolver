1238030ff5c6198e862b33f3e67acc1b7dc62015
amatgil <amatgilvinyes@gmail.com>
2025-04-01 12:58:23 +0200

    Improve nix config substantially

--- flake.nix
+++ flake.nix
@@ -1,23 +1,23 @@
 {
-  description = "Default Rust flake (casenc)";
+  description = "tubaitu";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
   outputs =
     { self, nixpkgs, rust-overlay }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      overlays = [(import rust-overlay) ];
       pkgs = (import nixpkgs { system = "x86_64-linux"; inherit overlays; });
-      overlays = [ (import rust-overlay) ];
     in
     {
+      packages = forAllSystems (system: {
+        default = pkgs.callPackage ./default.nix { inherit pkgs; };
+      });
       devShells = forAllSystems (system: {
-        default = pkgs.callPackage ./shell.nix { inherit pkgs overlays; };
+        default = pkgs.callPackage ./shell.nix { inherit pkgs; };
       });
     };
 }
--- shell.nix
+++ shell.nix
@@ -1,30 +1,24 @@
 {
   pkgs ? import <nixpkgs> { },
-  lib,
-  overlays
+  lib ? pkgs.lib,
 }:
 let
   packages = with pkgs; [
     rust-analyzer
     rustfmt
-    clippy
     mold
-    cmake
+    rust-bin.stable.latest.default
 
     pkg-config
     xorg.libX11
     libGL
     alsa-lib
     xorg.libXi
-
-    (rust-bin.stable.latest.default.override {
-      targets = [ "wasm32-unknown-unknown" ];
-    })
   ];
-
 in
 pkgs.mkShell {
   # Get dependencies from the main package
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
   nativeBuildInputs = packages;
   buildInputs = packages;
   env = {
@@ -32,4 +26,3 @@ pkgs.mkShell {
     LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
   };
 }
-
